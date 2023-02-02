aa8f06f466376a24c736b3eb4a3b044b8b9a9ff6
lokeshrevathi <lokeshrevathi07@gmail.com>
2023-02-02 19:57:36 +0530

    toto completed

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -3,7 +3,26 @@ document.getElementById("task-value").addEventListener("keydown", function(event
         addTask();
     }
 });
-// document.getElementById("remove-task").addEventListener("",);
+function removeTask(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+}
+function completedTask(element) {
+    if (element.checked) {
+        let elementContainer = element.parentNode.parentNode;
+        let elementDiv = element.parentNode;
+        let checkMark = document.createElement("i");
+        checkMark.className = "fa fa-check check-mark";
+        checkMark.id = "check-mark"
+        elementDiv.appendChild(checkMark);
+        let container = document.getElementById("container-two");
+        container.appendChild(elementContainer);
+    } else {
+        let elementContainer = element.parentNode.parentNode;
+        let container = document.getElementById("container");
+        container.appendChild(elementContainer);
+        element.parentNode.removeChild(element.parentNode.childNodes[1]);
+    }
+}
 function addTask() {
     let taskContainer = document.getElementById("container");
     let task = document.createElement("div");
@@ -15,8 +34,10 @@ function addTask() {
     task.className = "added-task";
     let taskInnerLeft = document.createElement("div");
     taskInnerLeft.className = "task-height task-left-side-sub";
-    let circleCheckBox = document.createElement("i");
-    circleCheckBox.className = "fa fa-circle-thin circle-checkbox";
+    let circleCheckBox = document.createElement("input");
+    circleCheckBox.setAttribute("type", "checkbox");
+    circleCheckBox.setAttribute("class", "checkbox-round");
+    circleCheckBox.setAttribute("onchange", "completedTask(this)");
     taskInnerLeft.appendChild(circleCheckBox);
     let taskInnerMiddle = document.createElement("div");
     taskInnerMiddle.className = "task-height task-right-side-sub";
@@ -26,14 +47,15 @@ function addTask() {
     taskName.appendChild(node);
     taskInnerMiddle.appendChild(taskName);
     let taskInnerRight = document.createElement("div");
-    taskInnerRight.className = "task-height add-task-sub";
+    taskInnerRight.className = "add-task-sub";
     taskInnerRight.id = "remove-task";
     let trashIcon = document.createElement("i");
     trashIcon.className = "fa fa-trash trash-align";
     taskInnerRight.appendChild(trashIcon);
+    taskInnerRight.setAttribute("onclick", "removeTask(this)");
     task.appendChild(taskInnerLeft);
     task.appendChild(taskInnerMiddle);
     task.appendChild(taskInnerRight);
     taskContainer.appendChild(task);
-    document.getElementById("task-value").innerHTML = null;
+    document.getElementById("task-value").value = "";
 }
