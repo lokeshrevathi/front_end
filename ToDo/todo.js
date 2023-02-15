f877b3aa6ca7b5c4524ad404f62eba956ac25752
lokeshrevathi <lokeshrevathi07@gmail.com>
2023-02-15 19:52:03 +0530

    search operation completed

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,27 +1,26 @@
 let toggleIcon;
 let checkedToggleicon;
-const taskUrl = 'http://localhost:8080/api/v1/task';
-const completedUrl = 'http://localhost:8080/api/v1/task/completed';
+let searchContainerShow;
+const taskUrl = 'http://localhost:8080/api/v1/task/';
+const completedUrl = 'http://localhost:8080/api/v1/task/completed/';
+const searchedTaskUrl = 'http://localhost:8080/api/v1/task';
 const postMethod = 'POST';
 const putMethod = 'PUT';
+let searchContainerConunt;
 const searchBar = document.getElementById("search-bar-id");
-
 window.addEventListener('load', function() {
     showTasks();
     showCompletedTask();
     disableSearchBar();
     toggleIcon = "saved";
+    searchContainerConunt = 0;
+    searchContainerShow = true;
 });
 document.getElementById("task-value").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         addTask();
     }
 });
-function eventListeners(element) {
-    element.addEventListener('click', function() {
-        editFunc(element);
-    });
-}
 function removeEventListeners(element) {
     element.removeEventListener('click', function() {
         editTask(element);
@@ -56,6 +55,20 @@ function enableSearchBar(element) {
     searchBar.focus();
     closeSearchBox.addEventListener('click', () => {
         disableSearchBar(closeSearchBox);
+        document.getElementById("searched-task").remove();
+        searchContainerConunt = 0;
+    });
+    searchBar.addEventListener('input', () => {
+        searchContainerConunt++;
+        if (searchContainerConunt == 1) {
+            createSearchContainer();
+        }
+        let searchDiv = document.getElementById("searched-task").childNodes[2].childNodes[0];
+        let sCount = searchDiv.childElementCount;
+        if (0 != sCount) {
+            searchDiv.innerHTML = '';
+        }
+        showSearchedTasks(searchBar.value);
     });
 }
 function editFunc(element, value) {
@@ -75,19 +88,21 @@ function editFunc(element, value) {
     saveIcon.addEventListener('click', function() {
         saveFunc(saveIcon, value);
     });
+    console.log(editDiv.previousSibling.childNodes[0]);
+    editDiv.previousSibling.childNodes[0].addEventListener('keypress', event => {
+        if (event.key === "Enter") {
+            saveFunc(saveIcon, value);
+        }
+    });
 }
 function saveFunc(element, value) {
     toggleIcon = "saved";
     let editDiv = element.parentNode;
-    // let tasks = Array.from(editDiv.parentNode.parentNode.childNodes);
-    // let position = tasks.indexOf(editDiv.parentNode);
+    let oldvalue = value.taskName;
     value.taskName = editDiv.previousSibling.childNodes[0].value;
-    // getTasks(taskUrl).then(result => {
-    //     let taskObj = result[position - 1];
-    //     taskObj.taskName = updatedTask;
-    //     postTask(taskObj, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
-    // });
-    postTask(value, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
+    if (oldvalue != value.taskName) {
+        postTask(value, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
+    }
     let editIcon = document.createElement("i");
     let editedTaskElement = editDiv.previousSibling.childNodes[0];
     if (editedTaskElement.value == "") {
@@ -100,7 +115,9 @@ function saveFunc(element, value) {
     editIcon.className = "fa fa-pencil trash-align";
     editDiv.removeChild(editDiv.childNodes[0]);
     editDiv.appendChild(editIcon);
-    eventListeners(editIcon);
+    editIcon.addEventListener('click', () => {
+        editFunc(editIcon, value);
+    });
 }
 function customPopup(element, value, taskStatus) {
     console.log(element);
@@ -137,7 +154,6 @@ function customPopup(element, value, taskStatus) {
     });
 }
 function removeTask(element, taskStatus) {
-    alert("remove");
     let tasks = Array.from(element.parentNode.parentNode.childNodes);
     let position = tasks.indexOf(element.parentNode);
     let url = (taskStatus == "redo") ? taskUrl : completedUrl;
@@ -150,13 +166,6 @@ function removeTask(element, taskStatus) {
     element.parentElement.remove();
     removeMidChild();
 }
-// function completedTask(element) {
-//     if (element.checked) {
-//         completed(element);
-//     } else {
-//         redo(element);
-//     }
-// }
 function midDiv() {
     let completedElementCount = document.getElementById("container-two").childElementCount;
     let containerMiddle = document.getElementById("container-middle");
@@ -213,16 +222,6 @@ function completed(element, value, taskStatus) {
     }
     value.completed = true;
     postTask(value, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
-    // let tasks = Array.from(elementDiv.parentNode.parentNode.childNodes);
-    // let position = tasks.indexOf(elementDiv.parentNode);
-    // console.log(position);
-    // getTasks(taskUrl).then(taskList => {
-    //     let taskObj = taskList[position];
-    //     console.log(taskObj);
-    //     taskObj.completed = true;
-    //     postTask(taskObj, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
-
-    // });
     elementContainer.childNodes[2].removeChild(elementContainer.childNodes[2].childNodes[0]);
     container.appendChild(elementContainer);
     midDiv();
@@ -240,20 +239,8 @@ function redo(element, value, taskStatus) {
     circleCheckBox.addEventListener('click', () => {
         completed(circleCheckBox, value);
     });
-    // let tasks = Array.from(elementDiv.parentNode.parentNode.childNodes);
-    // let position = tasks.indexOf(elementDiv.parentNode);
-    // console.log(position);
-    // let url = ("redo" == taskStatus) ? taskUrl : completedUrl;
-    // getTasks(url).then(taskList => {
-    //     let taskObj = taskList[position];
-    //     console.log(taskObj);
-    //     taskObj.completed = false;
-    //     postTask(taskObj, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
-
-    // });
     value.completed = false;
     postTask(value, "PUT").then(data => console.log("success", data)).catch(error => console.log("Error:", error));
-    // circleCheckBox.setAttribute("onclick", "completed(this)");
     editIcon.className = "fa fa-pencil trash-align";
     elementContainer.childNodes[0].appendChild(circleCheckBox);
     elementContainer.childNodes[2].appendChild(editIcon);
@@ -274,7 +261,6 @@ function hideElement(element) {
 }
 function addTask() {
     let addedTask = { taskName : document.getElementById("task-value").value};
-    let newTask = {};
     if (addedTask.taskName == "") {
         alert("Please add the task...!");
         return false;
@@ -308,6 +294,9 @@ function addLastTask() {
 function createTaskDiv(value, taskStatus) {
     checkedToggleicon = taskStatus;
     let taskContainer = document.getElementById("container");
+    taskContainer.appendChild(createTodoTaskDiv(value, taskStatus));
+}
+function createTodoTaskDiv(value, taskStatus) {
     let task = document.createElement("div");
     let taskInnerLeft = document.createElement("div");
     let circleCheckBox = document.createElement("span");
@@ -325,7 +314,6 @@ function createTaskDiv(value, taskStatus) {
     circleCheckBox.addEventListener('click', () => {
         completed(circleCheckBox, value, taskStatus);
     });
-    // circleCheckBox.setAttribute("onclick", "completed(this)");
     taskInnerLeft.appendChild(circleCheckBox);
     taskInnerMiddle.className = "task-right-side-sub";
     taskValue.id = "task-name";
@@ -347,13 +335,11 @@ function createTaskDiv(value, taskStatus) {
     taskInnerRight.addEventListener('click', () => {
         customPopup(taskInnerRight, 'Do you want to delete the task....!', taskStatus)
     });
-    // taskInnerRight.setAttribute("onclick", "customPopup(this, 'Do you want to delete the task....!')");
     task.appendChild(taskInnerLeft);
     task.appendChild(taskInnerMiddle);
     task.appendChild(editIconDiv);
     task.appendChild(taskInnerRight);
-    taskContainer.appendChild(task);
-    // eventListeners(editIcon);
+    return task;
 }
 function createCompletedTaskDiv(value, taskStatus) {
     checkedToggleicon = taskStatus;
@@ -390,7 +376,6 @@ function createCompletedTaskDiv(value, taskStatus) {
     taskInnerRight.addEventListener('click', () => {
         customPopup(taskInnerRight, 'Do you want to delete the task....!', taskStatus)
     })
-    // taskInnerRight.setAttribute("onclick", "customPopup(this, 'Do you want to delete the task....!')");
     task.appendChild(taskInnerLeft);
     task.appendChild(taskInnerMiddle);
     task.appendChild(editIconDiv);
@@ -408,10 +393,79 @@ async function postTask(data, methods) {
     });
     return response.json();
 }
-function getTasks(url) {
-    const response = fetch(url, {
+async function getTasks(url) {
+    const response = await fetch(url, {
         method: "GET",
     });
     // console.log(response);
-    return response.then(response => response.json());
+    return  response.json();
+}
+async function getTasksByName(name) {
+    let url = 'http://localhost:8080/api/v1/task?taskName=' + name;
+    const response = await fetch(url);
+    return response.json();
+}
+function createSearchContainer() {
+    let searchDiv = document.createElement("div");
+    let searchDivTop = document.createElement("div");
+    let searchDivMiddle = document.createElement("div");
+    let searchDivBottomOuter = document.createElement("div");
+    let searchDivBottom = document.createElement("div");
+    searchDiv.id = "searched-task";
+    searchDiv.className = "searched-task-container";
+    searchDivTop.className = "searched-task-container-top";
+    searchDivMiddle.className = "searched-task-container-middle";
+    searchDivBottomOuter.id = "searched-task-bottom-outer";
+    searchDivBottomOuter.className = "searched-task-container-bottom-outer-show";
+    searchDivBottom.id = "searched-task-bottom";
+    searchDivBottom.className = "searched-task-container-bottom";
+    searchDiv.appendChild(searchDivTop);
+    // searchDivMiddle.appendChild(navigateSearchedTasks());
+    searchDiv.appendChild(searchDivMiddle);
+    searchDivBottomOuter.appendChild(searchDivBottom);
+    searchDiv.appendChild(searchDivBottomOuter);
+    document.body.appendChild(searchDiv);
+    navigateSearchedTasks(searchDivMiddle);
+}
+function showSearchedTasks(value) {
+    let container = document.getElementById("searched-task-bottom");
+    getTasksByName(value).then(result => {
+        document.getElementById("navigate-count").innerText = result.length;
+        for (let i of result) {
+            container.appendChild(createTodoTaskDiv(i,"searched"));
+        }
+    })
+}
+function navigateSearchedTasks(element) {
+    let navigateDiv = document.createElement("div");
+    let iconDiv = document.createElement("div");
+    let textDiv = document.createElement("div");
+    let countDiv = document.createElement("div");
+    let dropDownIcon = document.createElement("i");
+    dropDownIcon.className = "fa fa-angle-down arrow-down";
+    navigateDiv.id = "searched-navigate";
+    navigateDiv.className = "searched-navigate-container";
+    iconDiv.className = "navigate-icon-container";
+    textDiv.className = "navigate-text-container";
+    countDiv.id = "navigate-count";
+    countDiv.className = "navigate-count-container";
+    textDiv.innerText = "Tasks";
+    iconDiv.appendChild(dropDownIcon);
+    navigateDiv.appendChild(iconDiv);
+    navigateDiv.appendChild(textDiv);
+    navigateDiv.appendChild(countDiv);
+    element.appendChild(navigateDiv);
+    navigateDiv.addEventListener('click', () => {
+        searchContainerShow = !searchContainerShow;
+        hideSearchContainer(element.nextSibling, dropDownIcon);
+    });
+}
+function hideSearchContainer(container, icon) {
+    if (searchContainerShow) {
+        container.className = "searched-task-container-bottom-outer-show";
+        icon.style.transform = "rotate(0deg)";
+    } else {
+        container.className = "searched-task-container-bottom-outer-hide";
+        icon.style.transform = "rotate(-90deg)";
+    }
 }
